4f909af4c9d9e2182360f15ebf6d06e097492dfd
Mrinmay <mrinmay21012204@gmail.com>
2026-06-06 22:09:31 +0530

    Time: 207 ms (84.63%), Space: 68.5 MB (5.32%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from collections import deque
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        arr = []
+        cur = head
+
+        while cur :
+            arr.append(cur.val)
+            cur = cur.next
+        if len(arr) == 0:
+            return None
+        Q = deque(sorted(arr))
+        res = ListNode(Q.popleft())
+        node = res
+        while Q :
+            new = ListNode(Q.popleft())
+            node.next = new
+            node = node.next
+        return res
+
+        
+
+        
