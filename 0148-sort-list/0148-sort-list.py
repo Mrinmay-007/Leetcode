da5cba1cf442934e9250fbd998626d9b32e74b2e
Mrinmay <mrinmay21012204@gmail.com>
2026-06-06 22:12:45 +0530

    Time: 35 ms (95.27%), Space: 51.2 MB (14.79%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -10,22 +10,37 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        arr = []
-        cur = head
+        # arr = []
+        # cur = head
 
-        while cur :
-            arr.append(cur.val)
-            cur = cur.next
-        if len(arr) == 0:
-            return None
-        Q = deque(sorted(arr))
-        res = ListNode(Q.popleft())
-        node = res
-        while Q :
-            new = ListNode(Q.popleft())
-            node.next = new
-            node = node.next
-        return res
+        # while cur :
+        #     arr.append(cur.val)
+        #     cur = cur.next
+        # if len(arr) == 0:
+        #     return None
+        # Q = deque(sorted(arr))
+        # res = ListNode(Q.popleft())
+        # node = res
+        # while Q :
+        #     new = ListNode(Q.popleft())
+        #     node.next = new
+        #     node = node.next
+        # return res
+
+# Method 2 ==================
+        arr = []
+        temp = head
+        while temp:
+            arr.append(temp.val)
+            temp = temp.next
+        arr.sort()
+        temp = head
+        i = 0
+        while temp:
+            temp.val = arr[i]
+            i += 1
+            temp = temp.next
+        return head
 
         
 
