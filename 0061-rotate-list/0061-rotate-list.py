9912e392525739dd649fc025f8c2cbf6ddcd31b2
Mrinmay <mrinmay21012204@gmail.com>
2026-06-07 22:15:41 +0530

    Time: 1 ms (27.92%), Space: 12.5 MB (48.47%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -48,5 +48,41 @@ class Solution(object):
         return list1.next
 
 
+# Method 2 ===============
 
+# class Solution(object):
+#     def rotateRight(self, head, k):
+#         if not head or not head.next or k == 0:
+#             return head
 
+#         # Find length and tail
+#         n = 1
+#         tail = head
+
+#         while tail.next:
+#             tail = tail.next
+#             n += 1
+
+#         # Reduce unnecessary rotations
+#         k = k % n
+
+#         if k == 0:
+#             return head
+
+#         # Make circular list
+#         tail.next = head
+
+#         # Find new tail
+#         steps = n - k - 1
+#         new_tail = head
+
+#         for _ in range(steps):
+#             new_tail = new_tail.next
+
+#         # New head
+#         new_head = new_tail.next
+
+#         # Break the circle
+#         new_tail.next = None
+
+#         return new_head
