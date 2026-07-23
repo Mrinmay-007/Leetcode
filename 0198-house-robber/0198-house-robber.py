9634687393671b64dab5498f063b7084564ca769
Mrinmay <mrinmay21012204@gmail.com>
2026-07-23 15:03:34 +0530

    Time: 3 ms (7.47%), Space: 12.6 MB (20.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,26 +1,18 @@
 class Solution(object):
     def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         n = len(nums)
-    
-        def money(i,memo={}): 
-            if i >= n :
+
+        def solve(i, memo={}):
+            if i >= n:
                 return 0
-            
+
             if i in memo:
                 return memo[i]
-                
-            take = max(
-                nums[i] + money(i+2,memo),
-                money(i+1,memo)
-                )
-            skip = money(i+1,memo)
-            
-            memo[i]= max(take,skip)
+
+            take = nums[i] + solve(i + 2, memo)
+            skip = solve(i + 1, memo)
+
+            memo[i] = max(take, skip)
             return memo[i]
 
-        return money(0)
-        
+        return solve(0)
