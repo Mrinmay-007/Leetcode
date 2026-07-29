a7fb9141e64f82955ee5bed231e2d1bf30f06e4f
Mrinmay <mrinmay21012204@gmail.com>
2026-07-29 21:32:07 +0530

    Time: 332 ms (5.59%), Space: 163.9 MB (7.83%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -5,48 +5,48 @@
 #         self.left = None
 #         self.right = None
 
-# class Solution(object):
-#     def lowestCommonAncestor(self, root, p, q):
-#         """
-#         :type root: TreeNode
-#         :type p: TreeNode
-#         :type q: TreeNode
-#         :rtype: TreeNode
-#         """
-#         self.ans = None 
-
-#         def dfs(node):
-#             if node is None :
-#                 return 0
-            
-#             left = dfs(node.left)
-#             right = dfs(node.right)
-#             own = 0
-
-#             if (node == p or node == q):
-#                 own = 1
-#             total = left + right + own 
-
-#             if total == 2 and self.ans  is None :
-#                 self.ans = node
-
-#             return  total
-
-#         dfs(root)
-#         return self.ans       
-
-
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
-        if root is None:
-            return None
-        if root == p or root == q:
-            return root
-        left = self.lowestCommonAncestor(root.left,p,q)
-        right = self.lowestCommonAncestor(root.right,p,q)
-        if left and right:
-            return root
-        elif left:
-            return left
-        else:
-            return right
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        self.ans = None 
+
+        def dfs(node):
+            if node is None :
+                return 0
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            own = 0
+
+            if (node == p or node == q):
+                own = 1
+            total = left + right + own 
+
+            if total == 2 and self.ans  is None :
+                self.ans = node
+
+            return  total
+
+        dfs(root)
+        return self.ans       
+
+
+# class Solution(object):
+#     def lowestCommonAncestor(self, root, p, q):
+#         if root is None:
+#             return None
+#         if root == p or root == q:
+#             return root
+#         left = self.lowestCommonAncestor(root.left,p,q)
+#         right = self.lowestCommonAncestor(root.right,p,q)
+#         if left and right:
+#             return root
+#         elif left:
+#             return left
+#         else:
+#             return right
